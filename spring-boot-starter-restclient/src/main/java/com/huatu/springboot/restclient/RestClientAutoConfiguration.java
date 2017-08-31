ec214e346dfdac02e9a68f50462e6cf9568f22f9
韩超 <yan_che@qq.com>
2017-08-31 17:24:56 +0800

    dev

--- spring-boot-starter-restclient/src/main/java/com/huatu/springboot/restclient/RestClientAutoConfiguration.java
+++ spring-boot-starter-restclient/src/main/java/com/huatu/springboot/restclient/RestClientAutoConfiguration.java
@@ -23,6 +23,7 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -49,86 +50,99 @@ import java.util.concurrent.TimeUnit;
 @EnableConfigurationProperties(RestClientConfig.class)
 public class RestClientAutoConfiguration {
 
-    private RestClientConfig config;
-    public RestClientAutoConfiguration(RestClientConfig restClientConfig){
-        this. config = restClientConfig;
-    }
 
-    @Bean
+    @Configuration
     @ConditionalOnClass(OkHttpClient.class)
-    @ConditionalOnMissingBean(OkHttpClient.class)
-    @ConditionalOnProperty(value = "restclient.okhttp.enabled",havingValue = "true",matchIfMissing = true)
-    public OkHttpClient okHttpClient(){
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        return builder.connectTimeout(config.getConnectTimeout(), TimeUnit.MILLISECONDS)
-                .writeTimeout(config.getWriteTimeout(),TimeUnit.MILLISECONDS)
-                .readTimeout(config.getReadTimeout(),TimeUnit.MILLISECONDS)
-                .connectionPool(new ConnectionPool(config.getMaxTotal(),config.getKeepAliveTime(), TimeUnit.MILLISECONDS))
-                .followRedirects(false)//不跟踪重定向
-                .build();
+    protected static class OkHttpClientConfiguration {
+        @Autowired
+        private RestClientConfig config;
+
+        @Bean
+        @ConditionalOnMissingBean(OkHttpClient.class)
+        @ConditionalOnProperty(value = "restclient.okhttp.enabled",havingValue = "true",matchIfMissing = false)
+        public OkHttpClient okHttpClient(){
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            return builder.connectTimeout(config.getConnectTimeout(), TimeUnit.MILLISECONDS)
+                    .writeTimeout(config.getWriteTimeout(),TimeUnit.MILLISECONDS)
+                    .readTimeout(config.getReadTimeout(),TimeUnit.MILLISECONDS)
+                    .connectionPool(new ConnectionPool(config.getMaxTotal(),config.getKeepAliveTime(), TimeUnit.MILLISECONDS))
+                    .followRedirects(false)//不跟踪重定向
+                    .build();
+        }
     }
 
 
-    @Bean
+    @Configuration
     @ConditionalOnClass(HttpClient.class)
-    @ConditionalOnMissingBean(HttpClient.class)
-    @ConditionalOnProperty(value = "restclient.okhttp.enabled",havingValue = "true",matchIfMissing = false)
-    public HttpClient httpClient()  throws Exception {
-        ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
+    protected static class ApacheHttpclientConfiguration {
+        @Autowired
+        private RestClientConfig config;
+        /**
+         * 无配置情况下，默认生成httpclient的客户端
+         * @return
+         * @throws Exception
+         */
+        @Bean
+        @ConditionalOnMissingBean(HttpClient.class)
+        @ConditionalOnProperty(value = "restclient.httpclient.enabled",havingValue = "true",matchIfMissing = true)
+        public HttpClient httpClient()  throws Exception {
+            ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
 
-        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(new TrustStrategy() {
-            @Override
-            public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                return true;
-            }
-        }).build();
-        LayeredConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
-        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create().register("http", plainsf).register("https", sslsf).build();
-        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
-        // 将最大连接数增加到200
-        cm.setMaxTotal(config.getMaxTotal());
-        // 将每个路由基础的连接增加到20
-        cm.setDefaultMaxPerRoute(config.getMaxPerRoute());
-
-        RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(config.getConnectRequestTimeout()).
-                setConnectTimeout(config.getConnectTimeout()).setSocketTimeout(config.getSocketTimeout()).build();
-
-        //请求重试处理
-        HttpRequestRetryHandler httpRequestRetryHandler = new HttpRequestRetryHandler() {
-            public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
-                if (executionCount >= 3) {// 如果已经重试了5次，就放弃
-                    return false;
-                }
-                if (exception instanceof NoHttpResponseException) {// 如果服务器丢掉了连接，那么就重试
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(new TrustStrategy() {
+                @Override
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
                     return true;
                 }
-                if (exception instanceof SSLHandshakeException) {// 不要重试SSL握手异常
-                    return false;
-                }
-                if (exception instanceof InterruptedIOException) {// 超时
-                    return false;
-                }
-                if (exception instanceof UnknownHostException) {// 目标服务器不可达
-                    return false;
-                }
-                if (exception instanceof ConnectTimeoutException) {// 连接被拒绝
-                    return false;
-                }
-                if (exception instanceof SSLException) {// ssl握手异常
-                    return false;
-                }
+            }).build();
+            LayeredConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
+            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create().register("http", plainsf).register("https", sslsf).build();
+            PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
+            // 将最大连接数增加到200
+            cm.setMaxTotal(config.getMaxTotal());
+            // 将每个路由基础的连接增加到20
+            cm.setDefaultMaxPerRoute(config.getMaxPerRoute());
 
-                HttpClientContext clientContext = HttpClientContext.adapt(context);
-                HttpRequest request = clientContext.getRequest();
-                // 如果请求是幂等的，就再次尝试
-                if (!(request instanceof HttpEntityEnclosingRequest)) {
-                    return true;
-                }
-                return false;
-            }
-        };
+            RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(config.getConnectRequestTimeout()).
+                    setConnectTimeout(config.getConnectTimeout()).setSocketTimeout(config.getSocketTimeout()).build();
 
-        HttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig).setConnectionManager(cm).setRetryHandler(httpRequestRetryHandler).build();
-        return httpClient;
+            //请求重试处理
+            HttpRequestRetryHandler httpRequestRetryHandler = new HttpRequestRetryHandler() {
+                public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
+                    if (executionCount >= 3) {// 如果已经重试了5次，就放弃
+                        return false;
+                    }
+                    if (exception instanceof NoHttpResponseException) {// 如果服务器丢掉了连接，那么就重试
+                        return true;
+                    }
+                    if (exception instanceof SSLHandshakeException) {// 不要重试SSL握手异常
+                        return false;
+                    }
+                    if (exception instanceof InterruptedIOException) {// 超时
+                        return false;
+                    }
+                    if (exception instanceof UnknownHostException) {// 目标服务器不可达
+                        return false;
+                    }
+                    if (exception instanceof ConnectTimeoutException) {// 连接被拒绝
+                        return false;
+                    }
+                    if (exception instanceof SSLException) {// ssl握手异常
+                        return false;
+                    }
+
+                    HttpClientContext clientContext = HttpClientContext.adapt(context);
+                    HttpRequest request = clientContext.getRequest();
+                    // 如果请求是幂等的，就再次尝试
+                    if (!(request instanceof HttpEntityEnclosingRequest)) {
+                        return true;
+                    }
+                    return false;
+                }
+            };
+
+            HttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig).setConnectionManager(cm).setRetryHandler(httpRequestRetryHandler).build();
+            return httpClient;
+        }
     }
+
 }
