249c76f1835b35149d5dbb38cf585533bf809443
韩超 <yan_che@qq.com>
2017-09-18 23:01:07 +0800

    update

--- spring-boot-web-register/src/main/java/com/huatu/springboot/web/listener/WebRegistListener.java
+++ spring-boot-web-register/src/main/java/com/huatu/springboot/web/listener/WebRegistListener.java
@@ -1,6 +1,7 @@
 package com.huatu.springboot.web.listener;
 
 import com.huatu.springboot.web.register.WebRegister;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
@@ -10,6 +11,7 @@ import org.springframework.context.event.ContextStoppedEvent;
  * @author hanchao
  * @date 2017/9/18 14:44
  */
+@Slf4j
 public class WebRegistListener implements ApplicationListener {
     private WebRegister webRegister;
 
@@ -20,12 +22,16 @@ public class WebRegistListener implements ApplicationListener {
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
         if(webRegister == null){
+            log.warn("null register find...");
             return;
         }
         //org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent 容器启动完成
+        log.info("catch spring event,{}",event.getClass());
         if(event instanceof ApplicationReadyEvent){
+            log.info("app start,regist...");
             webRegister.regist();
         }else if(event instanceof ContextStoppedEvent){
+            log.info("app start,stop...");
             webRegister.unregister();
         }
     }
